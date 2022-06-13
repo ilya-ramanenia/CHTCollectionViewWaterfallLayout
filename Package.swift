4b5c7e648dc63cbb139d66d4156df82683f4a433
ILYA Ramanenia <ruby.royd@gmail.com>
2022-06-13 22:44:49 +0200

    Set minimum iOS version to 12

--- Package.swift
+++ Package.swift
@@ -4,8 +4,8 @@ import PackageDescription
 let package = Package(
     name: "CHTCollectionViewWaterfallLayout",
     platforms: [
-        .iOS(.v9),
-        .tvOS(.v9)
+        .iOS(.v12),
+        .tvOS(.v12)
     ],
     products: [
         .library(name: "CHTCollectionViewWaterfallLayout", targets: ["CHTCollectionViewWaterfallLayout"]),
